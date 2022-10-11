6d41e60358992c3cbeaa6ac0104dc0f34d30ed82
Romanlo8716 <112902313+Romanlo8716@users.noreply.github.com>
2022-10-11 13:12:18 +0300

    Add files via upload

---
+++ Compilator/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rText = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.text = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rText
+            // 
+            this.rText.Location = new System.Drawing.Point(16, 36);
+            this.rText.Margin = new System.Windows.Forms.Padding(4);
+            this.rText.Name = "rText";
+            this.rText.Size = new System.Drawing.Size(435, 466);
+            this.rText.TabIndex = 0;
+            this.rText.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(456, 36);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Код";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.text,
+            this.type});
+            this.dataGridView1.Location = new System.Drawing.Point(652, 36);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(428, 466);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // text
+            // 
+            this.text.HeaderText = "Лексема";
+            this.text.MinimumWidth = 6;
+            this.text.Name = "text";
+            this.text.Width = 120;
+            // 
+            // type
+            // 
+            this.type.HeaderText = "Предварительный тип";
+            this.type.MinimumWidth = 6;
+            this.type.Name = "type";
+            this.type.Width = 120;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 538);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rText);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rText;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn text;
+        private System.Windows.Forms.DataGridViewTextBoxColumn type;
+    }
+}
+
---
+++ Compilator/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba1
+{
+    public partial class Form1 : Form
+    { 
+        public List<string> Chars = new List<string> {"*=","-=","+=", "<=",">=", "&", "&&", "*", "$", ";", "-", "+", "/", "{", "}", "(", ")", "=", "<", ">", ",", "()"}; 
+        public class ListWithDuplicates : List<KeyValuePair<string, string>>
+        {
+            public void Add(string key, string value)
+            {
+                var element = new KeyValuePair<string, string>(key, value);
+                this.Add(element);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+
+ 
+           string Code = rText.Text.TrimStart().TrimEnd();
+          
+            string buffer = "";
+            for(int i = 0; i < Code.Length; i++)
+            {
+                if (buffer != "")
+                {
+                    if (Convert.ToString(Code[i]).Trim().Equals(""))
+                    {
+                        addToDict(buffer);
+                        buffer = "";
+                    }
+                    else if (Chars.Contains(Convert.ToString(Code[i])))
+                    {
+                        if (Chars.Contains(Convert.ToString(buffer) + Convert.ToString(Code[i])))
+                        {
+                            buffer += Code[i];
+                        }
+                        else
+                        {
+                            addToDict(buffer);
+                            buffer = Convert.ToString(Code[i]); 
+                        }
+                    }
+                    else
+                    {
+                        if (Chars.Contains(Convert.ToString(buffer[0]))){
+                            addToDict(buffer);
+                            buffer = "";
+                        }
+                        buffer += Code[i];
+                    }
+                }
+                else
+                {
+                    buffer += Code[i];
+                }
+
+                if (i == Code.Length - 1)
+                {
+                    addToDict(buffer);
+                }
+
+            }
+
+            buffer = "";
+           
+        }
+
+        
+        public void PrintLeksem(ListWithDuplicates list)
+        {
+            foreach (KeyValuePair<string, string> dictItem in list)
+            {
+                this.dataGridView1.Rows.Add(dictItem.Key, dictItem.Value);
+
+            }
+        }
+
+        public void addToDict(string lexem)
+        {
+
+            if (int.TryParse(lexem, out int numericValue))
+            {
+                var list = new ListWithDuplicates();
+                list.Add(lexem, "L");
+                PrintLeksem(list);
+            }
+            else
+            {
+                
+                if (Chars.Contains(Convert.ToString(lexem)))
+                {
+                    var list = new ListWithDuplicates();
+                    list.Add(lexem, "R");
+                    PrintLeksem(list);
+                }
+                else
+                {
+                    if (lexem[0] >= 48 && lexem[0] <= 57)
+                    {
+                        MessageBox.Show($"Ошибка в лексеме: {lexem}", "Вывод лексем", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                        return;
+                    }
+                    else
+                    {
+                        var list = new ListWithDuplicates();
+                        list.Add(lexem, "I");
+                        PrintLeksem(list);
+                    }
+                }
+                
+            }
+        }
+
+        private void PrintText()
+        {
+            StreamReader sr = new StreamReader("C:\\3 курс\\Теория автоматов и формальных языков\\Курсовая работа\\Compilator.txt", Encoding.UTF8);
+            string text = sr.ReadToEnd();
+            rText.AppendText(text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PrintText();
+        }
+    }
+}
